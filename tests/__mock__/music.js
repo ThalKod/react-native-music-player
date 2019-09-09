952b1e8e67535c2c0a2a4eee0752965e2b76f0f4
ThalKod <tmarcelin@thal.tech>
2019-09-09 11:11:21 -0400

    add MusicSingle Test

---
+++ tests/__mock__/music.js
@@ -0,0 +1,23 @@
+export const musicData = [
+  {
+    title: "Test music title",
+    artist: "Artist",
+    imageUrl: "imageurl",
+    fileName: "Filename",
+    id: "0"
+  },
+  {
+    title: "Picasso Test",
+    artist: "Artist",
+    imageUrl: "imageurl",
+    fileName: "Filename",
+    id: "2"
+  },
+  {
+    title: "Test music title 2",
+    artist: "Artist",
+    imageUrl: "imageurl",
+    fileName: "FilenaS",
+    id: "3"
+  }
+];
